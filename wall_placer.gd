b0c549e35f4ab68d82b96907479fb6a878565928
MatthieuFontaine1 <matthieu.fontaine591@gmail.com>
2025-03-01 10:54:07 -0500

    feat: WallPlacer now places basic wall with lifetime

--- wall_placer.gd
+++ wall_placer.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
 const wall_rotation_possible_positions := 20
-
+const wall_life_time := 10
 
 var preview_sprite : Sprite2D
 var wall_container : Node2D
 
+var SC_wall_straight := preload("res://WallStraight.tscn")
 
 func _ready() -> void:
 	preview_sprite = get_node("WallPreviewSprite")
@@ -18,11 +19,7 @@ func _process(_delta: float) -> void:
 	
 	# Handling input: wall placing
 	if (Input.is_action_just_pressed("place_wall")):
-		var new_sprite = Sprite2D.new()
-		new_sprite.texture = preview_sprite.texture
-		new_sprite.position = preview_sprite.position
-		new_sprite.rotation = preview_sprite.rotation
-		wall_container.add_child(new_sprite)
+		_place_wall()
 	
 	# Handling input: wall rotating
 	var rotate_preview_direction := 0
@@ -33,4 +30,10 @@ func _process(_delta: float) -> void:
 	
 	if (rotate_preview_direction):
 		preview_sprite.rotation += rotate_preview_direction * ((2*PI) / wall_rotation_possible_positions)
-	
+
+func _place_wall():
+	var new_wall := SC_wall_straight.instantiate()
+	new_wall.position = preview_sprite.position
+	new_wall.rotation = preview_sprite.rotation
+	wall_container.add_child(new_wall)
+	new_wall.get_tree().create_timer(wall_life_time).timeout.connect(new_wall.initiate_wall_destroy)
