941aa136b34382849b611ddad54c8aae856e908e
MatthieuFontaine1 <matthieu.fontaine591@gmail.com>
2025-03-01 11:30:16 -0500

    feat: Add custom camera for level

--- wall_placer.gd
+++ wall_placer.gd
@@ -1,21 +1,25 @@
 extends Node2D
 
-const wall_rotation_possible_positions := 20
-const wall_life_time := 10
+const _wall_rotation_possible_positions := 20
+const _wall_life_time := 10
 
-var preview_sprite : Sprite2D
-var wall_container : Node2D
+var _preview_sprite : Sprite2D
+var _wall_container : Node2D
+var _level_camera : Camera2D
 
 var SC_wall_straight := preload("res://WallStraight.tscn")
 
 func _ready() -> void:
-	preview_sprite = get_node("WallPreviewSprite")
-	wall_container = get_node("WallContainer")
+	_preview_sprite = get_node("WallPreviewSprite")
+	_wall_container = get_node("WallContainer")
+	
+	_level_camera = get_node("../Camera2D")
+	assert(_level_camera, "Error: level camera not found")
 
 
 func _process(_delta: float) -> void:
 	# Following mouse
-	preview_sprite.position = get_viewport().get_mouse_position() - position
+	_preview_sprite.position = _level_camera.get_global_mouse_position() - position
 	
 	# Handling input: wall placing
 	if (Input.is_action_just_pressed("place_wall")):
@@ -29,11 +33,11 @@ func _process(_delta: float) -> void:
 		rotate_preview_direction = 1
 	
 	if (rotate_preview_direction):
-		preview_sprite.rotation += rotate_preview_direction * ((2*PI) / wall_rotation_possible_positions)
+		_preview_sprite.rotation += rotate_preview_direction * ((2*PI) / _wall_rotation_possible_positions)
 
 func _place_wall():
 	var new_wall := SC_wall_straight.instantiate()
-	new_wall.position = preview_sprite.position
-	new_wall.rotation = preview_sprite.rotation
-	wall_container.add_child(new_wall)
-	new_wall.get_tree().create_timer(wall_life_time).timeout.connect(new_wall.initiate_wall_destroy)
+	new_wall.position = _preview_sprite.position
+	new_wall.rotation = _preview_sprite.rotation
+	_wall_container.add_child(new_wall)
+	new_wall.get_tree().create_timer(_wall_life_time).timeout.connect(new_wall.initiate_wall_destroy)
