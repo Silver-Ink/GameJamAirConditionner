dab7b664d4986414c10082df2baf00e209f2d7f6
MatthieuFontaine1 <matthieu.fontaine591@gmail.com>
2025-03-01 17:48:16 -0500

    feat: Add cooldown on wall placement

--- wall_placer.gd
+++ wall_placer.gd
@@ -2,12 +2,14 @@ extends Node2D
 
 const _wall_rotation_possible_positions := 12
 const _wall_life_time := 10
+const _wall_place_cooldown := 2
 
 var _preview_wall : Wall
 var _wall_container : Node2D
 var _level_camera : Camera2D
 
 var _can_place_wall : bool = true
+var _is_place_in_cooldown : bool = false
 
 var SC_wall_straight := preload("res://WallStraight.tscn")
 
@@ -42,19 +44,37 @@ func _process(_delta: float) -> void:
 		_preview_wall.rotation += rotate_preview_direction * ((2*PI) / _wall_rotation_possible_positions)
 
 func _place_wall():
-	if (not _can_place_wall):
+	if ((not _can_place_wall) or _is_place_in_cooldown):
 		return
 	
+	# Create wall instance
 	var new_wall := SC_wall_straight.instantiate()
 	new_wall.position = _preview_wall.position
 	new_wall.rotation = _preview_wall.rotation
 	_wall_container.add_child(new_wall)
 	new_wall.get_tree().create_timer(_wall_life_time).timeout.connect(new_wall.initiate_wall_destroy)
+	
+	# Initiate cooldown on wall place
+	_handle_placing_cooldown(true)
+	get_tree().create_timer(_wall_place_cooldown).timeout.connect(_handle_placing_cooldown.bind(false))
 
 func _handle_wall_placeable(is_placeable : bool):
+	_can_place_wall = is_placeable
+	
+	if (_is_place_in_cooldown):
+		return # Cooldown effects override impossible placement effects
+		
 	if (is_placeable):
 		_preview_wall.modulate = Color(1,1,1,1)
 		_can_place_wall = true
 	else:
 		_preview_wall.modulate = Color(1,0,0,1)
 		_can_place_wall = false
+
+func _handle_placing_cooldown(is_in_cooldown : bool):
+	_is_place_in_cooldown = is_in_cooldown
+	
+	if (is_in_cooldown):
+		_preview_wall.modulate = Color(1,1,0,1)
+	else:
+		_handle_wall_placeable(_can_place_wall) # If there's no cooldown, check if there's impossible placement
