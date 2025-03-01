ad48ce2c9662eb0bff5cb03f9c13dff4797fdd5c
Breval Ferrari <breval.ferrari@fish.golf>
2025-03-01 18:22:50 -0500

    generic item scene

---
+++ item.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+enum Type {
+	APPLE,
+	FISH,
+	HEART,
+	BLOB,
+	SONIC,
+	SQUARE,
+	TRASH
+}
+
+@export var type: Type
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	get_children()[0].play(get_spritesheet_name())
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func get_spritesheet_name() -> String:
+	match type:
+		Type.APPLE:
+			return "apple"
+		Type.FISH:
+			return "fish"
+		Type.HEART:
+			return "heart"
+		Type.BLOB:
+			return "slimte"
+		Type.SONIC:
+			return "sonic"
+		Type.SQUARE:
+			return "square guy"
+		Type.TRASH:
+			return "transh"
+		_:
+			push_error("Unknown item type")
+			return ""
