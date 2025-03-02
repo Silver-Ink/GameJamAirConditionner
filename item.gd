31f9a2f77f7a7fdc59ab6cfeb6b3d95eb74383c5
Breval Ferrari <breval.ferrari@fish.golf>
2025-03-01 21:20:27 -0500

    items appearing

--- item.gd
+++ item.gd
@@ -11,17 +11,26 @@ enum Type {
 }
 
 @export var type: Type
-var anim: AnimatedSprite2D
+var anim: ItemSprite
+var appearing: bool = false
+var item_scale := 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	anim = get_children()[0]
+	get_children().filter(func(child): return child is Before).front().play("default")
+	anim = get_children().filter(func(child): return child is ItemSprite).front()
 	anim.play(get_spritesheet_name())
+	anim.scale = Vector2(item_scale, item_scale)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if appearing:
+		if item_scale < 1:
+			anim.scale = Vector2(item_scale, item_scale)
+			item_scale += 0.15
+		else:
+			appearing = false
 
 
 func get_spritesheet_name() -> String:
@@ -52,3 +61,8 @@ func destroy():
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Slime:
 		destroy()
+
+
+func _on_appear_animation_finished() -> void:
+	anim.visible = true
+	appearing = true
